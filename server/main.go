6939558bb5640e7344dbc51d8e9a3ae4a3b46528
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-07-28 16:04:37 +0500

    feat: added basic code for client server

---
+++ server/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	pb "demo/proto"
+	"google.golang.org/grpc"
+	_ "google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	_ "google.golang.org/grpc/status"
+	"log"
+	"net"
+	"sort"
+	"sync"
+)
+
+type UsersServer struct {
+	pb.UnimplementedUsersServer
+	users sync.Map
+}
+
+func (s *UsersServer) AddUser(ctx context.Context, in *pb.AddUserRequest) (*pb.AddUserResponse, error) {
+	var response pb.AddUserResponse
+
+	if _, ok := s.users.Load(in.User.Email); ok {
+		return nil, status.Errorf(codes.Aborted, "User with email %v doesn't exists")
+	} else {
+		s.users.Store(in.User.Email, in.User)
+	}
+	return &response, nil
+}
+
+func (s *UsersServer) ListUsers(ctx context.Context, in *pb.ListUsersRequest) (*pb.ListUsersResponse, error) {
+
+	var list []string
+
+	s.users.Range(func(key, _ interface{}) bool {
+		list = append(list, key.(string))
+		return true
+	})
+
+	sort.Strings(list)
+
+	offset := int(in.Offset)
+	end := int(in.Offset + in.Limit)
+	if end > len(list) {
+		end = len(list)
+	}
+
+	if offset >= end {
+		offset = 0
+		end = 0
+	}
+
+	response := pb.ListUsersResponse{
+		Count:  int32(len(list)),
+		Emails: list[offset:end],
+	}
+
+	return &response, nil
+}
+
+func (s *UsersServer) GetUser(ctx context.Context, in *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	var response pb.GetUserResponse
+
+	if user, ok := s.users.Load(in.Email); ok {
+		response.User = user.(*pb.User)
+	} else {
+		return nil, status.Errorf(codes.NotFound, "User with email %v doesn't exists")
+	}
+	return &response, nil
+}
+func (s *UsersServer) DelUser(ctx context.Context, in *pb.DelUserRequest) (*pb.DelUserResponse, error) {
+	var response pb.DelUserResponse
+
+	if _, ok := s.users.LoadAndDelete(in.Email); !ok {
+		return nil, status.Errorf(codes.NotFound, "User with email %v doesn't exists")
+	}
+	return &response, nil
+}
+func main() {
+
+	listen, err := net.Listen("tcp", ":3200")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUsersServer(s, &UsersServer{})
+
+	if err = s.Serve(listen); err != nil {
+		log.Fatal(err)
+	}
+}
